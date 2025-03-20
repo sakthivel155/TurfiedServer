ff7fff1f9ce851cf5e22d84917b26685f480ed6b
sakthivel155 <sakthivelramesh155@gmail.com>
2025-03-20 15:41:04 +0530

    reset backend

--- src/config/database.js
+++ src/config/database.js
@@ -5,8 +5,11 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // Important for cloud-hosted databases
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Use this in your models:
