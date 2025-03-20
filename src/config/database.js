b56c7e43ffa4d09e4998001d30236bf42d6c3d45
sakthivel155 <sakthivelramesh155@gmail.com>
2025-03-20 15:09:44 +0530

    DB correction

--- src/config/database.js
+++ src/config/database.js
@@ -5,11 +5,8 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Important for cloud-hosted databases
 });
 
 // Use this in your models:
